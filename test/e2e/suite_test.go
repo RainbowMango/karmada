3c2a5323b82953a47d1fb2cce311e22154e3ae0c
RainbowMango <renhongcai@huawei.com>
2020-12-01 15:18:58 +0800

    fix timeout

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -20,7 +20,7 @@ func TestE2E(t *testing.T) {
 	ginkgo.RunSpecs(t, "E2E Suite")
 }
 
-var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
+var _ = ginkgo.BeforeSuite(func() {
 	// suite set up, such as get karmada environment ready.
 }, TestSuiteSetupTimeOut.Seconds())
 
--- test/integration/suite_test.go
+++ test/integration/suite_test.go
@@ -20,7 +20,7 @@ func TestIntegration(t *testing.T) {
 	ginkgo.RunSpecs(t, "Integration Suite")
 }
 
-var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
+var _ = ginkgo.BeforeSuite(func() {
 	// suite set up, such as get karmada environment ready.
 }, TestSuiteSetupTimeOut.Seconds())
 
