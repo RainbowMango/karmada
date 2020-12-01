1fac9eb0c62e01b0d64f325887f77d35f6407366
RainbowMango <renhongcai@huawei.com>
2020-12-01 11:58:00 +0800

    squash me

---
+++ test/integration/work_test.go
@@ -0,0 +1,10 @@
+package integration
+
+import "github.com/onsi/ginkgo"
+
+var _ = ginkgo.Describe("Work", func() {
+	ginkgo.BeforeEach(func() {
+		// prepare work, such as initialize objects used by this test.
+	})
+
+})
